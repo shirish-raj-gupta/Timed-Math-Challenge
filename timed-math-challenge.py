09e7e228be888eb04146381ca7a426552e721809
shirish-raj-gupta <codersrg@gmail.com>
2025-06-12 08:41:46 +0530

    complete

---
+++ timed-math-challenge.py
@@ -0,0 +1,23 @@
+import random 
+
+OPERATORS = ['+','-','/','*']
+MIN_OPERAND = 3
+MAX_OPERAND = 12
+TOTAL_PROBLEMS = 10
+
+def generate_problem():
+  left = random.randint(MIN_OPERAND,MAX_OPERAND)
+  right = random.randint(MIN_OPERAND,MAX_OPERAND)
+  operator = random.choice(OPERATORS)
+  
+  expr = str(left)+ ' '+ operator + ' '+str(right)
+  answer = eval(expr)
+  return expr ,answer
+
+
+for i in range(TOTAL_PROBLEMS):
+  expr, answer = generate_problem()
+  while True:
+    guess = input('Problem #'+ str(i+1)+':'+expr+'=')
+    if guess == str(answer):
+      break

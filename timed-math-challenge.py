b7f324f17c4412dee6490669f4fae34393e3fef0
shirish-raj-gupta <codersrg@gmail.com>
2025-06-12 09:03:10 +0530

    complete

--- timed-math-challenge.py
+++ timed-math-challenge.py
@@ -1,4 +1,5 @@
 import random 
+import time
 
 OPERATORS = ['+','-','/','*']
 MIN_OPERAND = 3
@@ -15,9 +16,23 @@ def generate_problem():
   return expr ,answer
 
 
+wrong = 0
+input('Enter enter to start!')
+print('-----------------------')
+
+start_time = time.time()
+
 for i in range(TOTAL_PROBLEMS):
   expr, answer = generate_problem()
   while True:
     guess = input('Problem #'+ str(i+1)+':'+expr+'=')
     if guess == str(answer):
       break
+    wrong+=1
+
+
+end_time = time.time()
+total_time = end_time - start_time
+
+print('-----------------------')
+print('Nice Work! You finished in', total_time,'seconds!')
